977c15776ab370a2b495cda7634b44f980d0344b
Tahirah Roohi <tahirahroohi0@gmail.com>
2025-01-01 14:13:42 +0500

    resolved errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,6 @@ import React from "react";
 import Home from "./home/page";
 import products from "../data/products";
 
-interface Product {
-  name: string;
-  description: string;
-  price: string;
-  originalPrice?: string;
-  discount?: string;
-  image: string;
-}
-
 export default function Main() {
   return (
     <div>
