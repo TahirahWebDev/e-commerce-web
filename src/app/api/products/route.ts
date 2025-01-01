61aee7b68e4a9f395fc4b9e5e2a5b699cf8f041a
Tahirah Roohi <tahirahroohi0@gmail.com>
2025-01-01 14:25:32 +0500

    resolved errors

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import products from "../../../data/products";
 
+// Use named exports for HTTP methods (GET, POST, etc.)
 export async function GET() {
   return NextResponse.json(products);
 }
