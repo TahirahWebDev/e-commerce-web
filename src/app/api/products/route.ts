13a360318f70c62f413df1a0f0b9c5ad22eba200
Tahirah Roohi <tahirahroohi0@gmail.com>
2025-01-01 14:17:55 +0500

    resolved errors

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,6 +1,6 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 import products from "../../../data/products";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(products);
+export async function GET() {
+  return NextResponse.json(products);
 }
